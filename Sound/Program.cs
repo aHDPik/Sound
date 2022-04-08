da6a3f02ff8e67c08938b9be353fed550f6c637e
Андрей Рудзитис <andreyrud@inbox.lv>
2022-04-09 00:13:46 +0300

    sound example

---
+++ Sound/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Media;
+
+namespace Sound
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = Environment.CurrentDirectory+ "\\bell.wav";
+            player.Play();
+            Console.ReadLine();
+        }
+    }
+}
